a9a5716e7543bc5a36e480e8b3ccc61b17fc6eda
Rafael dos Santos Giorgi <106035150+devraffles@users.noreply.github.com>
2024-12-24 12:17:39 -0300

    Update index.tsx

--- src/app/dashboard/product/components/form/index.tsx
+++ src/app/dashboard/product/components/form/index.tsx
@@ -80,71 +80,75 @@ export function Form({ categories }: Props ){
   
   
     return(
-      <main className={styles.container}>
-        <h1>Novo produto</h1>
-  
-        <form className={styles.form} action={handleRegisterProduct}>
-  
-          <label className={styles.labelImage}>
-            <span>
-              <UploadCloud size={30} color="#FFF" />
-            </span>
-  
-            <input 
-              type="file" 
-              accept="image/png, image/jpeg"
-              required
-              onChange={handleFile}
-            />
-  
-  
-            {previewImage && (
-              <Image
-                alt="Imagem de preview"
-                src={previewImage}
-                className={styles.preview}
-                fill={true}
-                quality={100}
-                priority={true}
-              />
-            )}
-  
-          </label>
-  
-          <select name="category">
-            {categories.map( (category, index) => (
-              <option key={category.id} value={index}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-  
-          <input 
-            type="text" 
-            name="name"
-            placeholder="Digite o nome do produto..."
-            required
-            className={styles.input}
-          />
-  
-          <input 
-            type="text" 
-            name="price"
-            placeholder="Preço do produto..."
-            required
-            className={styles.input}
-          />        
-  
-          <textarea
-            className={styles.input}
-            placeholder="Digite a descrição do produto..."
-            required
-            name="description"
-          ></textarea>
-  
-          <Button name="Cadastrar produto" />
-  
-        </form>
-      </main>
+        <>
+            <div>
+                <main className={styles.container}>
+                    <h1>Novo produto</h1>
+              
+                    <form className={styles.form} action={handleRegisterProduct}>
+              
+                      <label className={styles.labelImage}>
+                        <span>
+                          <UploadCloud size={30} color="#FFF" />
+                        </span>
+              
+                        <input 
+                          type="file" 
+                          accept="image/png, image/jpeg"
+                          required
+                          onChange={handleFile}
+                        />
+              
+              
+                        {previewImage && (
+                          <Image
+                            alt="Imagem de preview"
+                            src={previewImage}
+                            className={styles.preview}
+                            fill={true}
+                            quality={100}
+                            priority={true}
+                          />
+                        )}
+              
+                      </label>
+              
+                      <select name="category">
+                        {categories.map( (category, index) => (
+                          <option key={category.id} value={index}>
+                            {category.name}
+                          </option>
+                        ))}
+                      </select>
+              
+                      <input 
+                        type="text" 
+                        name="name"
+                        placeholder="Digite o nome do produto..."
+                        required
+                        className={styles.input}
+                      />
+              
+                      <input 
+                        type="text" 
+                        name="price"
+                        placeholder="Preço do produto..."
+                        required
+                        className={styles.input}
+                      />        
+              
+                      <textarea
+                        className={styles.input}
+                        placeholder="Digite a descrição do produto..."
+                        required
+                        name="description"
+                      ></textarea>
+              
+                      <Button name="Cadastrar produto" />
+              
+                    </form>
+                  </main>
+            </div>
+        </>
     )
   }
